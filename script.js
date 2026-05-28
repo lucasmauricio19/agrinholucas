1aab78db646c70d71756ae6e180b268b0f44b24e
agenda-caicavv <163298358+lucasmauricio19@users.noreply.github.com>
2026-05-28 13:59:59 -0300

    s

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-const topoBtn = document.getElementById("topoBtn");
+const imagens = document.querySelectorAll("img");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
-    topoBtn.classList.add("mostrar");
-  } else {
-    topoBtn.classList.remove("mostrar");
-  }
-});
-
-topoBtn.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
+imagens.forEach((imagem) => {
+  imagem.addEventListener("dragstart", (evento) => {
+    evento.preventDefault();
   });
 });
