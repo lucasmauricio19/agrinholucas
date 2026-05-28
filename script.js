ea474fbc51aafa4fd51dde1af8182e13c9b2f1df
agenda-caicavv <163298358+lucasmauricio19@users.noreply.github.com>
2026-05-28 10:53:41 -0300

    s

--- script.js
+++ script.js
@@ -1,47 +1,16 @@
-const topoBtn = document.getElementById('topoBtn');
+const topoBtn = document.getElementById("topoBtn");
 
-window.addEventListener('scroll', () => {
-
-  if(window.scrollY > 300) {
-    topoBtn.style.display = 'block';
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    topoBtn.classList.add("mostrar");
   } else {
-    topoBtn.style.display = 'none';
+    topoBtn.classList.remove("mostrar");
   }
-
 });
 
-topoBtn.addEventListener('click', () => {
-
+topoBtn.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth'
+    behavior: "smooth"
   });
-
-});
-
-const sections = document.querySelectorAll('section');
-
-const observer = new IntersectionObserver(entries => {
-
-  entries.forEach(entry => {
-
-    if(entry.isIntersecting) {
-      entry.target.style.opacity = '1';
-      entry.target.style.transform = 'translateY(0px)';
-    }
-
-  });
-
-}, {
-  threshold: 0.2
-});
-
-sections.forEach(section => {
-
-  section.style.opacity = '0';
-  section.style.transform = 'translateY(50px)';
-  section.style.transition = 'all 1s ease';
-
-  observer.observe(section);
-
 });
